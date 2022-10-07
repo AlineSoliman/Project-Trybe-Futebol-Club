c52fb12fd331f98eb8f20541135154f230756fad
Aline Soliman <alineapreis@hotmail.com>
2022-10-07 16:38:13 -0300

    feat: cria model de matches

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,56 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+import Teams from './TeamsModel';
+
+class Match extends Model {
+  id: number;
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: number;
+}
+
+Match.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    homeTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    homeTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeam: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    awayTeamGoals: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    inProgress: {
+      type: INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'matches',
+    timestamps: false,
+  },
+);
+Teams.hasMany(Match, { foreignKey: 'id', as: 'matches' });
+
+Match.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+
+Match.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
