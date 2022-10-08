99d32dee5cc638d592a00ff958e8f93a63ad49b3
Aline Soliman <alineapreis@hotmail.com>
2022-10-08 08:34:59 -0300

    feat: cria validação para Token

---
+++ app/backend/src/Service/Validations/TokenValidate.ts
@@ -0,0 +1,23 @@
+import { JwtPayload, sign, verify } from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import { IJwtPayload } from '../../Interface/IjwtPayload';
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET || 'segredo';
+
+export default class Jwt {
+  static async generateToken(payload: JwtPayload): Promise<string> {
+    const token: string = sign(payload, secret);
+    return token;
+  }
+
+  static validateToken = async (token: string) => {
+    try {
+      const valid = verify(token, secret) as IJwtPayload;
+      return valid.role;
+    } catch (error) {
+      throw new Error('Token must be a valid token');
+    }
+  };
+}
