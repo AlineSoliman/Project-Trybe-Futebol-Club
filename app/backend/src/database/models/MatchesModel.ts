a825ea1377595dafd0a3927d2cffac5b65bc7c27
Aline Soliman <alineapreis@hotmail.com>
2022-10-08 08:59:39 -0300

    feat: comenta arquivos de matches para tentar rodar avaliador pois quebrou testes

--- app/backend/src/database/models/MatchesModel.ts
+++ app/backend/src/database/models/MatchesModel.ts
@@ -1,56 +1,56 @@
-import { INTEGER, Model } from 'sequelize';
-import db from '.';
-import Teams from './TeamsModel';
+// import { INTEGER, Model } from 'sequelize';
+// import db from '.';
+// import Teams from './TeamsModel';
 
-class Match extends Model {
-  id: number;
-  homeTeam: number;
-  homeTeamGoals: number;
-  awayTeam: number;
-  awayTeamGoals: number;
-  inProgress: number;
-}
+// class Match extends Model {
+//   id: number;
+//   homeTeam: number;
+//   homeTeamGoals: number;
+//   awayTeam: number;
+//   awayTeamGoals: number;
+//   inProgress: number;
+// }
 
-Match.init(
-  {
-    id: {
-      type: INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    homeTeam: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    homeTeamGoals: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    awayTeam: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    awayTeamGoals: {
-      type: INTEGER,
-      allowNull: false,
-    },
-    inProgress: {
-      type: INTEGER,
-      allowNull: false,
-    },
-  },
-  {
-    underscored: true,
-    sequelize: db,
-    modelName: 'matches',
-    timestamps: false,
-  },
-);
-Teams.hasMany(Match, { foreignKey: 'id', as: 'matches' });
+// Match.init(
+//   {
+//     id: {
+//       type: INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true,
+//     },
+//     homeTeam: {
+//       type: INTEGER,
+//       allowNull: false,
+//     },
+//     homeTeamGoals: {
+//       type: INTEGER,
+//       allowNull: false,
+//     },
+//     awayTeam: {
+//       type: INTEGER,
+//       allowNull: false,
+//     },
+//     awayTeamGoals: {
+//       type: INTEGER,
+//       allowNull: false,
+//     },
+//     inProgress: {
+//       type: INTEGER,
+//       allowNull: false,
+//     },
+//   },
+//   {
+//     underscored: true,
+//     sequelize: db,
+//     modelName: 'matches',
+//     timestamps: false,
+//   },
+// );
+// Teams.hasMany(Match, { foreignKey: 'id', as: 'matches' });
 
-Match.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+// Match.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
 
-Match.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+// Match.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
 
-export default Match;
+// export default Match;
