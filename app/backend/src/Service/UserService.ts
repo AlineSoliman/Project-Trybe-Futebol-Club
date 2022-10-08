2ccb350a1e3e24293576a1823689c1ccd4c7bb76
Aline Soliman <alineapreis@hotmail.com>
2022-10-08 08:35:17 -0300

    feat: cria camada service para user

---
+++ app/backend/src/Service/UserService.ts
@@ -0,0 +1,37 @@
+import User from '../database/models/UserModel';
+import Bcrypt from './Validations/Bcrypt';
+import TokenValidate from './Validations/TokenValidate';
+import ILogin from '../Interface/ILogin';
+
+export default class UserService {
+  public static login = async (infoLogin: ILogin) => {
+    console.log(infoLogin.email);
+    const { email, password } = infoLogin;
+    const userData = await User.findOne({
+      where: { email },
+    });
+    if (!userData) {
+      return { code: 401, message: 'Incorrect email or password' };
+    }
+
+    if (!Bcrypt.comparePassword(password, userData.password)) {
+      return { code: 401, message: 'Incorrect email or password' };
+    }
+
+    const { id, role } = userData;
+    const payload = { id, role };
+
+    const token = await TokenValidate.generateToken(payload);
+    return token;
+  };
+
+  public static getAllUsers = async () => {
+    const findUser = await User.findAll();
+    return findUser;
+  };
+
+  public static verifyUser = async (token: string) => {
+    const validation = TokenValidate.validateToken(token);
+    return validation;
+  };
+}
