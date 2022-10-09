6cdfa13c37edf7247da634ed23dcea93f2189b49
Aline Soliman <alineapreis@hotmail.com>
2022-10-09 11:41:11 -0300

    feat: cria camada matches na camada controllers

---
+++ app/backend/src/Controllers/MatchesController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import MatchService from '../Service/MatchesService';
+
+export default class MatchController {
+  public static async getAll(request: Request, response: Response) {
+    const { inProgress } = request.query;
+    let result = [];
+    if (typeof inProgress === 'string') {
+      result = await MatchService.getAllProgress(
+        JSON.parse(inProgress),
+      );
+    } else {
+      result = await MatchService.getAll();
+    }
+    response.status(200).json(result);
+  }
+
+  public static async updateMatch(request: Request, response: Response) {
+    const { id } = request.params;
+    const result = await MatchService.updateMatch(Number(id), request.body);
+    response.status(200).json(result);
+  }
+
+  public static async getAllProgress(
+    inProgress: string,
+    response: Response,
+  ) {
+    const result = await MatchService.getAllProgress(inProgress);
+    response.status(200).json(result);
+  }
+  // falta fórmula para salvar a partida
+}
