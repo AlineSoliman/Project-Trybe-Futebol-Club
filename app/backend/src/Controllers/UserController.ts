63e7d2fd4dcd5c8ee700fce74669f1c7e1e04470
Aline Soliman <alineapreis@hotmail.com>
2022-10-09 12:55:59 -0300

    feat: altera retorno de try catch

--- app/backend/src/Controllers/UserController.ts
+++ app/backend/src/Controllers/UserController.ts
@@ -1,21 +1,23 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import UserService from '../Service/UserService';
 
 // interface newError {
 //   message: string;
+//   notice: string;
+//   statusCode: number;
 // }
 export default class UserController {
-  public login = async (req: Request, res: Response) => {
+  public login = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const token = await UserService.login(req.body);
       if (req.body.email === '' || req.body.password === '') {
         return res.status(400).send({ message: 'All fields must be filled' });
       }
-      if (token) return res.status(200).json({ token });
+      if (!token) return res.status(401).json({ message: 'Incorrect email or password' });
+
+      return res.status(200).json(token);
     } catch (error) {
-      // const { message } = error as newError;
-      // console.log(message);
-      return res.status(500).json({ message: 'Erro' });
+      next(error);
     }
   };
 
