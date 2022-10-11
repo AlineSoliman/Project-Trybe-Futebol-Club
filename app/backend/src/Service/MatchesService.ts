54226d22f7dd53fb41dc81dea8d641a1adec0ba6
Aline Soliman <alineapreis@hotmail.com>
2022-10-11 07:15:07 -0300

    feat: inclui funções para atualizar, criar e para partidas finalizadas na camada service de Matches

--- app/backend/src/Service/MatchesService.ts
+++ app/backend/src/Service/MatchesService.ts
@@ -1,6 +1,5 @@
 import Match from '../database/models/MatchesModel';
 import Teams from '../database/models/teamsModel';
-// import { IMatch } from '../Interface/IMatches';
 
 export default class MatchService {
   public static getAll = async () => {
@@ -24,5 +23,25 @@ export default class MatchService {
     return matches;
   };
 
-  //  falta fórmula de salvar a partida
+  public static createMatch = async (body: any) => {
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = body;
+    const verifyHomeTeam = await Teams.findByPk(homeTeam);
+    const verifyAwayTeam = await Teams.findByPk(awayTeam);
+    if (!verifyAwayTeam || !verifyHomeTeam) {
+      return false;
+    }
+    const createMatches = await Match.create({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals });
+    return createMatches;
+  };
+
+  public static finish = async (id: string) => {
+    const updateMatches = Match.update({ inProgress: false }, { where: { id } });
+    return updateMatches;
+  };
+
+  public static updateMatch = async (id: string, { homeTeamGoals, awayTeamGoals }: any) => {
+    const updateInProgressMatches = Match
+      .update({ homeTeamGoals, awayTeamGoals }, { where: { id } });
+    return updateInProgressMatches;
+  };
 }
